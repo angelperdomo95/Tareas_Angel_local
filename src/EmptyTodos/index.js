6728073e8b868849e2284cebdfcbf42325a9e03d
angelperdomo95 <mi_lo2009@hotmail.es>
2023-08-09 15:33:54 -0500

    Updates

--- src/EmptyTodos/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-function EmptyTodos() {
-  return(
-    <p> Empieza Tu Primer TODO! </p>
-  );
-}
-  export{ EmptyTodos };
--- src/TodosError/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-//import './TodosError.css';
-
-function TodosError() {
-  return(
-    <p>ups!! Error!</p>
-  );
-}
-  export{TodosError};
